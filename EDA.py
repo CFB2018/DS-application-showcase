31be922e5db2e91d5e3c8461a5d0021211316f6e
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-21 15:34:06 +0100

    Load dataset and import libraries

---
+++ EDA.py
@@ -0,0 +1,21 @@
+
+'''
+Objective: Load the Spacex_dataset into  a table in the Db2 database
+Includes a record for each payload carried during a SpaceX mission.
+Execute queries to understand the dataset.
+
+'''
+
+import pandas as pd
+
+# load the data
+df = pd.read_csv('spacex_web_scraped.csv')
+
+
+# Connect to the database
+import csv, sqlite3
+import prettytable
+prettytable.DEFAULT = 'DEFAULT'
+
+con = sqlite3.connect("my_data1.db")
+cur = con.cursor()
