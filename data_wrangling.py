9b76dc5952a27cf7836c394c2d7adf2cb02efc59
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-21 13:20:27 +0100

    explain the objective

---
+++ data_wrangling.py
@@ -0,0 +1,11 @@
+
+'''
+Objective: perform exploratory data analysis to find patterns in the data.
+Booster landing outcomes; 
+True Ocean = mission outcome was successful
+False Ocean = mission outcome was not successful
+True RTLS or False RTLS , True ASDs or False ASDS
+
+These outcomes will be converted into training labels; 1 = booster successfully landed, 
+and 2 = booster landing was unsuccessful.
+'''
