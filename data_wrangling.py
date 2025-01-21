0ebf99a233f74c71f526c908130512c2589be497
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-21 13:23:17 +0100

    update objective

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,11 +1,10 @@
 
 '''
-Objective: perform exploratory data analysis to find patterns in the data.
-Booster landing outcomes; 
-True Ocean = mission outcome was successful
-False Ocean = mission outcome was not successful
+Objective: Perform exploratory data analysis to find patterns in the data and determine training labels.
+
+Booster landing outcomes will be converted into training labels; 1 = booster landed successfully, 
+and 2 = booster landing was unsuccessful.
+True Ocean (mission outcome was successful), or False Ocean (mission outcome was not successful),
 True RTLS or False RTLS , True ASDs or False ASDS
 
-These outcomes will be converted into training labels; 1 = booster successfully landed, 
-and 2 = booster landing was unsuccessful.
 '''
