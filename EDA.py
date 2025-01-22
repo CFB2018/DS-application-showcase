ac50dcde650b985b5945fc8fb3f815acacbcdf98
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-22 10:45:19 +0100

    connect to database and load the data

--- EDA.py
+++ EDA.py
@@ -1,21 +1,38 @@
-
-'''
-Objective: Load the Spacex_dataset into  a table in the Db2 database
-Includes a record for each payload carried during a SpaceX mission.
-Execute queries to understand the dataset.
-
-'''
+# Objective: Load the Spacex_dataset into a table in the Db2 database
+# Includes a record for each payload carried during a SpaceX mission.
+# Execute queries to understand the dataset.
 
 import pandas as pd
+import sqlite3
+import prettytable
+
+prettytable.DEFAULT = 'DEFAULT'
+
+# Connect to the database
+con = sqlite3.connect("spaceX-data.db")
+cur = con.cursor()
 
 # load the data
 df = pd.read_csv('spacex_web_scraped.csv')
+df.to_sql('SPACEXTBL', con, if_exists = 'replace', index = False, method = 'multi')
 
 
-# Connect to the database
-import csv, sqlite3
-import prettytable
-prettytable.DEFAULT = 'DEFAULT'
 
-con = sqlite3.connect("spaceX-data.db")
-cur = con.cursor()
+
+
+
+'''
+# Example: Execute a query
+cur.execute("SELECT * FROM your_table_name")
+
+# Fetch the results
+results = cur.fetchall()
+
+# Print the results
+for row in results:
+    print(row)
+
+# Close the connection
+con.close()
+
+'''
