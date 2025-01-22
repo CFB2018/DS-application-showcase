2501e4af9777edc4db60a896c76a0eea2e01ee24
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-22 11:18:31 +0100

    check that df was inserted in sql table

--- EDA.py
+++ EDA.py
@@ -1,38 +1,35 @@
 
+# SQL NOTEBOOK
 # Objective: Load the Spacex_dataset into a table in the Db2 database
 # The data includes a record for each payload carried during a SpaceX mission.
 # Execute queries to understand the dataset better.
 
-
-import sqlite3
+import sqlalchemy
+import csv, sqlite3
 import pandas as pd
 import prettytable
-
 prettytable.DEFAULT = 'DEFAULT'
 
-# Connect to sqlite database
+# Connect to sqlite database spacex
 conn = sqlite3.connect("spacex.db")
 
 # Cursor class can invoke methods that execute SQLite statements
 cur = conn.cursor()
 
-# Create a table in the database
 # Drop the table if it already exists.
-cur.execute("DROP TABLE IF EXISTS spacex")
+cur.execute("DROP TABLE IF EXISTS STABLE")
 
-# Creating table
-table = """ create table IF NOT EXISTS INSTRUCTOR(ID INTEGER PRIMARY KEY NOT NULL, FNAME VARCHAR(20), LNAME VARCHAR(20), CITY VARCHAR(20), CCODE CHAR(2));"""
-cur.execute(table)
+# Creating table STABLE
+STABLE = """ create table IF NOT EXISTS INSTRUCTOR(ID INTEGER PRIMARY KEY NOT NULL, FNAME VARCHAR(20), LNAME VARCHAR(20), CITY VARCHAR(20), CCODE CHAR(2));"""
+cur.execute(STABLE)
 print("Table is Ready")
 
-
-'''
 # load the data
 df = pd.read_csv('spacex_web_scraped.csv')
 df.to_sql('spacex', conn, if_exists = 'replace', index = False, method = 'multi')
 
 
-# Example: Execute a query
+# Execute a query
 cur.execute("SELECT * FROM spacex")
 
 # Fetch the results
@@ -43,6 +40,4 @@ for row in results:
     print(row)
 
 # Close the connection
-con.close()
-
-'''
+conn.close()
