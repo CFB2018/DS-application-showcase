3e22af779ba5aa4472aa4777431a017a979c7a0e
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-23 16:36:04 +0100

    Plot Flightnumber vs PayloadMass

--- EDA_viz.py
+++ EDA_viz.py
@@ -13,3 +13,9 @@ import seaborn as sns
 # Load the data from CSV
 df = pd.read_csv('dataset_part_2.csv')
 print(df.head(5))
+
+# Plot Flightnumber vs PayloadMass
+sns.catplot(y="PayloadMass", x="FlightNumber", hue="Class", data=df, aspect = 5)
+plt.xlabel("Flight Number",fontsize=20)
+plt.ylabel("Pay load Mass (kg)",fontsize=20)
+plt.show()
