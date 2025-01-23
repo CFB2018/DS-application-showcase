7cb2bdd84ead075f9354198d0cf567731346442c
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-23 16:34:31 +0100

    import libraries and load data

---
+++ EDA_viz.py
@@ -0,0 +1,15 @@
+
+'''
+Exploratory Data Analysis with Visualization
+
+'''
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data from CSV
+df = pd.read_csv('dataset_part_2.csv')
+print(df.head(5))
