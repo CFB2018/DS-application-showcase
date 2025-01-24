aa09217ff51bd209f22b514b176466cbbf641055
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-24 14:59:01 +0100

    flight number per launch site

--- EDA_viz.py
+++ EDA_viz.py
@@ -18,8 +18,17 @@ print(df.head(5))
 g = sns.catplot(y="PayloadMass", x="FlightNumber", hue="Class", data=df, aspect = 2)
 g.fig.suptitle("Payload Mass by Flight Number", fontsize= 20)
 g.set_xticklabels(rotation=45, ha='right', fontsize=10)
+x_ticks = np.arange(0, len(df['FlightNumber'].unique()), step=2)
 g.set_axis_labels("Flight Number", "Payload Mass (kg)", fontsize=14)
+plt.xticks(ticks=x_ticks, fontsize=10)
 plt.tight_layout()
 plt.show()
 
-# Relationship between Flight number and Launch site
+# View the relationship between Flight number and Launch site
+plt.figure(figsize=(12, 6))
+sns.countplot(y="LaunchSite", data=df, order=df['LaunchSite'].value_counts().index)
+plt.title("Number of Flights by Launch Site", fontsize=20)
+plt.xlabel("Number of Flights", fontsize=14)
+plt.ylabel("Launch Site", fontsize=14)
+plt.tight_layout()
+plt.show()
