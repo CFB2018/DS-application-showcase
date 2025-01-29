ea86709aadf503e98a3d1f2d0359bfa5405a097a
Carmen Flores Bjurström <cflbjurstrom@gmail.com>
2025-01-29 19:36:47 +0100

    Add callback function / piechart  per site

--- dash_interactivity.py
+++ dash_interactivity.py
@@ -4,8 +4,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, Input
 import pandas as pd
-
+import plotly.express as px
 
 # Load the data from CSV
 df = pd.read_csv('spacex_launch_geo.csv')
@@ -30,9 +31,40 @@ app.layout = html.Div([
         placeholder = 'Select a Launch Site',
         searchable=True,
         clearable=False
-    )
+    ),
+    html.Br(),  # Space between dropdown and chart
+
+    dcc.Graph(id='success-pie-chart') 
 ])
+
+
+# Add a callback function to render success-pie-chart based on selected site dropdown
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+               Input(component_id='site-dropdown', component_property='value'))
+
+# Add computation to callback function and return graph
+def get_piechart(entered_site):
+    if entered_site == 'ALL':
+        # Group by 'Launch Site' and count successes
+        success_counts = df[df['class'] == 1].groupby('Launch Site').size().reset_index(name='count')
+
+        fig = px.pie(success_counts, values='count', names='Launch Site',
+                     title='Total Successful Launches (All Sites)')
+    
+    else:
+        # Filter for the selected site
+        filtered_df = df[df['Launch Site'] == entered_site]
+        success_counts = filtered_df['class'].value_counts().reset_index()
+        success_counts.columns = ['class', 'count']
+
+        fig = px.pie(success_counts, values='count', names='class',
+                     title=f'Success vs Failure for {entered_site}')
+    
+    return fig
 
 # Run the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True, port=8060)
+
+
+# Open a web and type http://127.0.0.1:8060/ to see the Dashboard
